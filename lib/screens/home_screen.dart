8501d3d530d29c02205791f6cd6079f27569a957
Kartika Tri Juliana <kartikatjuliana@gmail.com>
2025-10-22 10:54:23 +0700

    UTS : Basis Awal Proyek OCR Sederhana

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Menu Utama')),
+      appBar: AppBar(title: const Text('Ini Menu Utama')),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
