892f5b1fd65bc02aa6e93cf27494e2ac6d2b9aa9
Kartika Tri Juliana <kartikatjuliana@gmail.com>
2025-10-22 13:26:47 +0700

    UTS: Selesai Soal 1 - ListTile dan Navigasi Balik

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 class ResultScreen extends StatelessWidget {
   final String ocrText;
 
-  // Constructor yang memerlukan ocrText
   const ResultScreen({super.key, required this.ocrText});
 
   @override
@@ -14,15 +14,23 @@ class ResultScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: SelectableText(
-            // Tampilkan pesan error jika ocrText kosong,
-            // atau tampilkan teks hasil OCR.
-            // Mengganti '\n' dengan spasi untuk tampilan yang lebih rapi jika diperlukan.
-            ocrText.isEmpty
-                ? 'Tidak ada teks ditemukan.'
-                : ocrText.replaceAll('\n', ' '),
+            // Tampilkan teks utuh tanpa replaceAll
+            ocrText.isEmpty ? 'Tidak ada teks ditemukan.' : ocrText,
             style: const TextStyle(fontSize: 18),
           ),
         ),
+      ),
+
+      // Tambahkan FloatingActionButton
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (_) => const HomeScreen()),
+            (route) => false, // menghapus semua halaman sebelumnya
+          );
+        },
+        child: const Icon(Icons.home),
       ),
     );
   }
